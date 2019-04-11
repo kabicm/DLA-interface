6e50936ef09442def7f5e132edc4996c82e95f5c
Marko Kabic <marko.kabic@cscs.ch>
2019-04-11 16:14:26 +0200

    Modifying the matrix multiplication for benchmarking and linking it to mpiP profiler.

--- install_scalapack.sh
+++ install_scalapack.sh
@@ -15,4 +15,5 @@ FC=ftn cmake -DHWLOC_ROOT=$EBROOTHWLOC -DDLA_LAPACK_TYPE="MKL" -DDLA_SCALAPACK_T
 make -j 10
 
 #OMP_NUM_THREADS=18 MKL_NUM_THREADS=18 srun -N 2 --ntasks-per-node=2 --cpu-bind=verbose,sockets -p -y ./miniapp/matrix_multiplication -m 10000 -n 10000 -k 10000 --scalapack -r 3 -p 2 -q 2
+# flags -p and -y are the flags of mpiP library for profiling
 
